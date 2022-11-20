5889fd8b228c6e9e2913cb2563caa2035ab6fd48
Lunal <100156578+AlexeyLunyakov@users.noreply.github.com>
2022-11-20 14:26:35 +0300

    Delete old_pr directory

--- old_pr/pr2_done/TestAuthor.java
+++
@@ -1,8 +0,0 @@
-package git.old_pr.pr2_done;
-
-public class TestAuthor {
-    public static void main(String[] args) {
-        Author a = new Author("Иван Иванов", "ivan@mirea.ru", 'm');
-        System.out.println(a);
-    }
-}
--- old_pr/pr3_done/ex5/Movable.java
+++
@@ -1,8 +0,0 @@
-package git.old_pr.pr3_done.ex5;
-
-public interface Movable {
-    public abstract void moveUp();
-    public abstract void moveDown();
-    public abstract void moveLeft();
-    public abstract void moveRight();
-}
