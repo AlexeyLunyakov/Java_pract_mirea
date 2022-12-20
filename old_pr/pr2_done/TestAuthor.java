258cc283b39238e4a4ca0d19ffdef652011ee4eb
Lunal <100156578+AlexeyLunyakov@users.noreply.github.com>
2022-12-20 21:32:55 +0300

    Update TestAuthor.java

--- old_pr/pr2_done/TestAuthor.java
+++ old_pr/pr2_done/TestAuthor.java
@@ -1,8 +1,9 @@
-package git.old_pr.pr2_done;
+package ru.mirea.old_pr.pr2_done;
 
 public class TestAuthor {
     public static void main(String[] args) {
-        Author a = new Author("Иван Иванов", "ivan@mirea.ru", 'm');
+        Author a = new Author("Луняков Алексей", "lunyakov_aa@exmpl.ru", 'm');
         System.out.println(a);
     }
 }
+
