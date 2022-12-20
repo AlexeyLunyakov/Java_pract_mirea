e7f9957b91f7c4fa81730ac199680bbb53631e7e
Lunal <100156578+AlexeyLunyakov@users.noreply.github.com>
2022-12-20 21:28:14 +0300

    Update TestBook.java

--- old_pr/pr1_done/TestBook.java
+++ old_pr/pr1_done/TestBook.java
@@ -5,9 +5,10 @@ public class TestBook {
         Book b1 = new Book();
         System.out.println("Книга, созданная конструктором по умолчанию:");
         System.out.println(b1);
-        Book b2 = new Book("Война и мир", "Лев Николаевич Толстой");
+        Book b2 = new Book("Война и мир", "Лев Николаевич Толстой", "Издательство", 1, 1900);
         System.out.println("Книга, созданная конструктором с частичной инициализацией:");
         System.out.println(b2);
     }
 }
+
 
