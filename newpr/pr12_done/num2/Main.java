c98c51ce36ba53d7749479d761f2884e111f1140
Lunal <100156578+AlexeyLunyakov@users.noreply.github.com>
2022-12-21 23:40:57 +0300

    Add files via upload

---
+++ newpr/pr12_done/num2/Main.java
@@ -0,0 +1,7 @@
+package ru.mirea.newpr.pr12_done.num2;
+
+public class Main {
+    public static void main(String[] args){
+        new Window();
+    }
+}
---
+++ newpr/pr12_done/num3/Main.java
@@ -0,0 +1,7 @@
+package ru.mirea.newpr.pr12_done.num3;
+
+public class Main {
+    public static void main(String [] args){
+        new Animation();
+    }
+}
